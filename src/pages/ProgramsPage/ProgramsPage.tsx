e336444eb82a9bba376da983f039b46743e6cadb
alexis <alexisrk310@gmail.com>
2024-06-19 21:00:08 -0500

    fixed error url images in page programas

--- src/pages/ProgramsPage/ProgramsPage.tsx
+++ src/pages/ProgramsPage/ProgramsPage.tsx
@@ -5,8 +5,8 @@ import { motion } from 'framer-motion';
 const ProgramsPage = ({}) => {
 	const images = [
 		'https://encolombia.com/wp-content/uploads/2020/05/comunidades-negras.jpg',
-		'https://www.senado.gov.co/images/afro.jpg',
 		'https://www.cancilleria.gov.co/sites/default/files/newsroom/news/images/afro.jpg',
+		'https://i0.wp.com/www.ucaldas.edu.co/portal/wp-content/uploads/2020/07/comunidad-afro.jpg?fit=700%2C400&ssl=1',
 	];
 	return (
 		<>
