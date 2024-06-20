827022fe02532d15c51208495836a42a31e09456
alexis <alexisrk310@gmail.com>
2024-06-19 19:59:22 -0500

    fixed errors import, variables declaradas pero no utilizadas

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,93 +1,8 @@
-import {
-	AuroraBackground,
-	BentoGrid,
-	BentoGridItem,
-	WaveMask,
-} from '@/components';
-import { AiFillQuestionCircle } from 'react-icons/ai';
+import { AuroraBackground } from '@/components';
+
 import { motion } from 'framer-motion';
 
 const HomePage = ({}) => {
-	// const items = [
-	// 	{
-	// 		title: 'The Dawn of Innovation',
-	// 		description: 'Explore the birth of groundbreaking ideas and inventions.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// 	{
-	// 		title: 'The Digital Revolution',
-	// 		description: 'Dive into the transformative power of technology.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// 	{
-	// 		title: 'The Art of Design',
-	// 		description: 'Discover the beauty of thoughtful and functional design.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// 	{
-	// 		title: 'The Power of Communication',
-	// 		description:
-	// 			'Understand the impact of effective communication in our lives.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br im"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// 	{
-	// 		title: 'The Pursuit of Knowledge',
-	// 		description: 'Join the quest for understanding and enlightenment.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// 	{
-	// 		title: 'The Joy of Creation',
-	// 		description: 'Experience the thrill of bringing ideas to life.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// 	{
-	// 		title: 'The Spirit of Adventure',
-	// 		description: 'Embark on exciting journeys and thrilling discoveries.',
-	// 		header: (
-	// 			<img
-	// 				src="https://afrocaribebcn.net/wp-content/uploads/2023/01/cropped-logotipo-afrocaribe-sin-fondo.png"
-	// 				className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br"
-	// 			/>
-	// 		),
-	// 		icon: <AiFillQuestionCircle className="h-4 w-4 text-neutral-500" />,
-	// 	},
-	// ];
 	return (
 		<div>
 			<AuroraBackground>
@@ -116,13 +31,8 @@ const HomePage = ({}) => {
 					</div>
 				</motion.div>
 			</AuroraBackground>
-			
-			
 		</div>
 	);
 };
 
-// const Skeleton = () => (
-// 	<div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
-// );
 export default HomePage;
